0521c46756fb77e3f4758bad2fceff98a2851bf2
criiss31 <44347444+criiss31@users.noreply.github.com>
2018-12-04 13:36:56 +0000

    created 3 multiple canvases and reused the clock class

--- design.js
+++ design.js
@@ -2,7 +2,7 @@ class Clock{
 	constructor() {
 		
 	} 
-	
+ 
 	setBackgroundFill(backgroundfill) {
 		this.backgroundfill = backgroundfill;
 	}
@@ -31,159 +31,266 @@ class Clock{
 		return this.clocktype;
 	}
 	
-
 	
-	
-	draw() {
-		translate(width / 2.0, height / 2.0);
+	draw(p, hoursOffset) {
 
+		p.translate(p.width / 2.0, p.height / 2.0);
+
+		
 		var date = new Date(),
-		hours = date.getHours(),
+		hours = date.getHours() + hoursOffset,
 		minutes = date.getMinutes(),
 		seconds = date.getSeconds(),
 		ms = date.getMilliseconds(),
+		day = date.getUTCDate(),
+		month = date.getMonth(),
+		year = date.getUTCFullYear(),
+		
 		i,
 		v,
 		t; 
 		 
-		var bf = this.backgroundfill;
-		background(bf);
+		p.background(this.getBackgroundFill());
 
 		// Draw clock
-		var cf = this.clockfill;
-		fill(cf);
-		stroke(0);
-		strokeWeight(4); 
-		ellipse(0, 0, 450, 450);
+		p.fill(this.getClockFill());
+		p.stroke(0);
+		p.strokeWeight(8); 
+		p.ellipse(0, 0, 300, 300);
 		
 		// Minute Markers
-		fill(cf);
-		strokeWeight(1);
+		p.fill(255);
+		p.strokeWeight(1);
+	
 		for (i = 0; i < 60; i++) {
-			v = p5.Vector.fromAngle((i + 1) / 60.0 * TAU - HALF_PI);
-			v.mult(210);
+			v = p5.Vector.fromAngle((i + 1) / 60.0  * p.TAU - p.HALF_PI);
+			v.mult(115);
+			if ((i+1) %3 == 0) {
+				p.fill('red');
+			}
+			else {
+				p.fill(255);
+			}
 			if (i % 5 == 4) {
-				ellipse(v.x, v.y, 5, 5);
+				p.ellipse(v.x, v.y, 5, 5);
 			} 
 			else {
-				ellipse(v.x, v.y, 1, 1);
+				p.ellipse(v.x, v.y, 1, 1);
 			}
 		}
 		
-		 // Numbers
-		textSize(36);
-		var tf = this.textfill;
-		fill(tf);
-		noStroke()
+		
+		// Numbers
+		p.textSize(25);
+		p.fill(0);
+		p.noStroke();
 		for (i = 0; i < 12; i++) {
-			v = p5.Vector.fromAngle((i + 1) / 12.0 * TAU - HALF_PI);
-			v.mult(180);
-			var ct = this.clocktype;
+			v = p5.Vector.fromAngle((i + 1) / 12.0 * p.TAU - p.HALF_PI);
+			v.mult(100);
+			var ct = this.getClockType();
+			//var ct = 'a';
 			if (ct == 'a') {
-				text(i + 1, v.x, v.y);
+				p.text(i + 1, v.x-8, v.y+10);
 			}
 			else {
 				switch(i + 1) {
 					case 1: 
-						text('I' ,v.x, v.y);
+						p.text('I', v.x-8, v.y+10);
 						break;
 					case 2:
-						text('II' ,v.x, v.y);
+						p.text('II', v.x-8, v.y+10);
 						break;
 					case 3:
-						text('III' ,v.x, v.y);
+						p.text('III', v.x-8, v.y+10);
 						break;
 					case 4:
-						text('IV' ,v.x, v.y);
+						p.text('IV', v.x-8, v.y+10);
 						break;
 					case 5:
-						text('V' ,v.x, v.y);
+						p.text('V', v.x-8, v.y+10);
 						break;
 					case 6:
-						text('VI' ,v.x, v.y);
+						p.text('VI', v.x-8, v.y+10);
 						break;
 					case 7:
-						text('VII' ,v.x, v.y);
+						p.text('VII', v.x-8, v.y+10);
 						break;
 					case 8:
-						text('VIII' ,v.x, v.y);
+						p.text('VIII', v.x-8, v.y+10);
 						break;
 					case 9:
-						text('IX' ,v.x, v.y);
+						p.text('IX', v.x-8, v.y+10);
 						break;
 					case 10:
-						text('X' ,v.x, v.y);
+						p.text('X',v.x-8, v.y+10);
 						break;
 					case 11:
-						text('XI' ,v.x, v.y);
+						p.text('XI', v.x-8, v.y+10);
 						break;
 					case 12:
-						text('XII' ,v.x, v.y);
+						p.text('XII', v.x-8, v.y+10);
 						break;
 				}
 			}
 		}
 		
-		  // Hour hand
-		stroke(0);
-		strokeWeight(4);
-		t = (hours + minutes / 60 + seconds / 3600) * TAU / 12 - HALF_PI;
+		// Numbers for minutes
+		p.textSize(10);
+		for (i = 0; i < 12; i++) {
+			v = p5.Vector.fromAngle((i + 1) / 12.0 * p.TAU - p.HALF_PI);
+			v.mult(130);
+			if (i == 11){
+				p.text("0", v.x, v.y);
+			}
+			else {
+				p.text((i + 1)*5, v.x, v.y);
+			}
+		}
+		
+		
+				
+		// Date box
+		p.textSize(13);
+		
+		p.fill(120);
+		p.stroke(0);
+		p.strokeWeight(1);
+		p.rect(-40, 30, 85, 30, 10);
+		
+		p.fill(0);
+		p.noStroke();
+		p.text(day, -30, 50);
+		p.text(".", -15, 50);
+		p.text(month+1, -15, 50);
+		p.text(".", 6, 50);
+		p.text(year, 10, 50);
+		
+		//AM/PM box
+		p.fill(120); 
+		p.stroke(0);
+		p.strokeWeight(1);
+		p.rect(-18, -42, 35, 22, 10);
+		
+		p.fill(0);
+		p.noStroke();
+		if (hours >=0 && hours <=11) {
+			p.text("AM", -10, -25);
+		}
+		else {
+			p.text("PM", -10, -25);
+		}
+		
+		
+		
+		// Hour hand
+		p.stroke(0);
+		p.strokeWeight(3);
+		t = (hours + minutes / 60 + seconds / 3600) * p.TAU / 12 - p.HALF_PI;
 		v = p5.Vector.fromAngle(t);
-		v.mult(127);
-		line(0, 0, v.x, v.y);
+		v.mult(60);
+		p.line(0, 0, v.x, v.y); 
 
 		// Minute hand
-		strokeWeight(2);
-		t = (minutes + seconds / 60 + ms / 1000 / 60) * TAU / 60 - HALF_PI;
+		p.strokeWeight(2);
+		t = (minutes + seconds / 60 + ms / 1000 / 60) * p.TAU / 60 - p.HALF_PI;
 		v = p5.Vector.fromAngle(t);
-		v.mult(205);
-		line(0, 0, v.x, v.y);
+		v.mult(112);
+		p.line(0, 0, v.x, v.y); 
 
 		// Second hand
-		stroke(255, 0, 0);
-		strokeWeight(1);
-		t = (seconds + ms / 1000 / 60) * TAU / 60 - HALF_PI;
+		p.stroke(255, 0, 0);
+		p.strokeWeight(1);
+		t = (seconds + ms / 1000 / 60) * p.TAU / 60 - p.HALF_PI;
 		//t = (seconds + ms / 1000) * TAU / 60 - HALF_PI;
 		v = p5.Vector.fromAngle(t);
-		v.mult(210);
-		line(0, 0, v.x, v.y);
+		v.mult(115);
+		p.line(0, 0, v.x, v.y); 
 		
 		// Center cap
-		fill(0);
-		stroke(0);
-		noStroke();
-		ellipse(0, 0, 20, 20);
+		p.fill(0);
+		p.stroke(0);
+		p.noStroke();
+		p.ellipse(0, 0, 10, 10); 
 
-		resetMatrix();
+		p.resetMatrix();
 	}
 	
 	
 }
 
+var sketch1 = function(p) {
+	p.x = 100;
+	p.y = 100;
+	var c = new Clock();
+	p.setup = function() {
+		p.createCanvas(500, 500);
+		p.textFont('Futura, Avenir, Helvetica, Georgia, Sans-Serif');
+		
+		c.setBackgroundFill(192);
+		c.setClockFill(255);
+		c.setTextFill(0);
+		c.setClockType('a');
+		
+	}
+	p.draw = function() {	
+		c.draw(p, 7);
+	}
+		p.getClock = function() {	
+		return c;
+	}
+}
 
-var c;
-
-
-function setup() {
-	createCanvas(1200, 750);
-	textAlign(CENTER, CENTER);
-	strokeCap(SQUARE);
-	textFont('Futura, Avenir, Helvetica, Georgia, Sans-Serif');
-	
-	c = new Clock();
-	c.setBackgroundFill(192);
-	c.setClockFill(255);
-	c.setTextFill(0);
-	c.setClockType('r');
+var sketch2 = function(p) {
+	p.x = 100;
+	p.y = 100;
+	var c = new Clock();
+	p.setup = function() {
+		p.createCanvas(500, 500);
+		p.textFont('Futura, Avenir, Helvetica, Georgia, Sans-Serif');
+		
+		c.setBackgroundFill(192);
+		c.setClockFill(255);
+		c.setTextFill(0);
+		c.setClockType('a');
+		
+	}
+	p.draw = function() {	
+		c.draw(p, 0);
+	}
+	p.getClock = function() {	
+		return c;
+	}
 	
 	
 }
-
-function draw() {
-	c.draw();
+var sketch3 = function(p) {
+	p.x = 100;
+	p.y = 100;
+	var c = new Clock();
+	p.setup = function() {
+		p.createCanvas(500, 500);
+		p.textFont('Futura, Avenir, Helvetica, Georgia, Sans-Serif');
+		
+		c.setBackgroundFill(192);
+		c.setClockFill(255);
+		c.setTextFill(0);
+		c.setClockType('a');
+	}
+	p.draw = function() {	
+		c.draw(p, -5);
+	}
+	p.getClock = function() {	
+		return c;
+	}
 	
-} 
+}
 
-function changeColor(){
-	c.setBackgroundFill(0);
+var c1 = new p5(sketch1);
+var c2 = new p5(sketch2);
+var c3 = new p5(sketch3);
+
+function changeClockType() {
+	c1.getClock().setClockType('r');
+	c2.getClock().setClockType('r');
+	c3.getClock().setClockType('r');
 }
